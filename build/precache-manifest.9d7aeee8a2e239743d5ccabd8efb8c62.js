ecba9210046cd7eb2949896b558340e59c67dc5b
Saad Hmed <saad.ahmed2696@gmail.com>
2020-06-17 02:36:34 +0500

    intial commit

---
+++ build/precache-manifest.9d7aeee8a2e239743d5ccabd8efb8c62.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375cd2abf9a3cde35c1d92ad1e9b55f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3587e72e54cdfc1d35c",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "44a47f559ab8a0ae5e0c",
+    "url": "/static/css/main.106effe9.chunk.css"
+  },
+  {
+    "revision": "f3587e72e54cdfc1d35c",
+    "url": "/static/js/2.5dff0d40.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5dff0d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a47f559ab8a0ae5e0c",
+    "url": "/static/js/main.a4e9cf0e.chunk.js"
+  },
+  {
+    "revision": "b35897691b1089d43a2c",
+    "url": "/static/js/runtime-main.a81ebef7.js"
+  },
+  {
+    "revision": "a285359d99e118b778227dab0d3c696d",
+    "url": "/static/media/ad.a285359d.jpg"
+  },
+  {
+    "revision": "12990617d5afd67d5c711f35dfe86b95",
+    "url": "/static/media/felxo-inks.12990617.jpg"
+  },
+  {
+    "revision": "8fc59baa22220dde87ef214a435ee426",
+    "url": "/static/media/film.8fc59baa.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a3f76e60c3d172ff89d11f4bb938554f",
+    "url": "/static/media/gcy.a3f76e60.jpeg"
+  },
+  {
+    "revision": "48a4a9642d98924c2d1dc4dabac680db",
+    "url": "/static/media/homepg.48a4a964.jpg"
+  },
+  {
+    "revision": "3586ee5747e4b4c2fc8533e7a1cd2a45",
+    "url": "/static/media/inkind.3586ee57.jpg"
+  },
+  {
+    "revision": "fa73b787d087c707333c31ec1dbbae1c",
+    "url": "/static/media/pic1.fa73b787.jpeg"
+  },
+  {
+    "revision": "c83e0472798b87ab7e55fd394d18b794",
+    "url": "/static/media/print.c83e0472.jpg"
+  },
+  {
+    "revision": "68b1ff68132c4f8d170f7a937c9f5eeb",
+    "url": "/static/media/printer.68b1ff68.jpeg"
+  },
+  {
+    "revision": "378c8076a5aba693c67603b843f8b117",
+    "url": "/static/media/reg-cylinder.378c8076.jpg"
+  },
+  {
+    "revision": "b2ea433652f6bbf23b283f27cec94381",
+    "url": "/static/media/saad2.b2ea4336.jpg"
+  },
+  {
+    "revision": "8a850083b0c707ce5324b7cce234d34b",
+    "url": "/static/media/swiftlogo.8a850083.png"
+  },
+  {
+    "revision": "9123b58e3d7b4d6dc75b968dacfc23ae",
+    "url": "/static/media/waseem.9123b58e.jpg"
+  }
+]);
